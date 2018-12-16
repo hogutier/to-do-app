092c397c069c7ae733d68bf4b6388529d39ad750
Horacio Gutierrez <hogutier@yahoo.com>
2018-12-15 20:33:32 -0600

    Complete Todo Functionality and Styling

--- app.js
+++ app.js
@@ -6,48 +6,61 @@ function onReady() {
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
-    // get the text
-    let title = newToDoText.value;
+    if (newToDoText.value) {
+      // get the text
+      let title = newToDoText.value;
 
-    // create new li
-    let newLi = document.createElement('li');
+      // create new li
+      let newLi = document.createElement('li');
 
-    // create new input
-    let checkbox = document.createElement('input');
+      // create new input
+      let checkbox = document.createElement('input');
 
-    // set the input's type to checkbox
-    checkbox.type = 'checkbox';
+      // set the input's type to checkbox
+      checkbox.type = 'checkbox';
 
-    // set the title
-    newLi.textContent = title;
+      checkbox.classList.add('checkboxLi');
 
-    // attach the checkbox to the li
-    newLi.prepend(checkbox);
+      // set the title
+      newLi.textContent = title;
 
-    // create delete button
-    let deleteButton = document.createElement('button');
+      // attach the checkbox to the li
+      newLi.prepend(checkbox);
 
-    // create text node
-    let buttonText = document.createTextNode('Delete Item');
+      newLi.classList.add('lisAdded');
 
-    // attach text to button
-    deleteButton.appendChild(buttonText);
+      // create delete button
+      let deleteButton = document.createElement('button');
 
-    newLi.id = newLi.textContent;
+      // create text node
+      let buttonText = document.createTextNode('Delete Item');
 
-    // attach button to newLi
-    newLi.appendChild(deleteButton);
+      // attach text to button
+      deleteButton.appendChild(buttonText);
 
-    //attach the li to the ul
-    toDoList.appendChild(newLi);
+      newLi.id = newLi.textContent;
 
-    // empty the input
-    newToDoText.value = '';
+      newLi.classList.add('mdl-list__item');
+      newLi.classList.add('mdl-list__item-primary-content');
 
-    deleteButton.addEventListener('click', event => {
-      let liToRemove = event.target.parentNode;
-      liToRemove.remove();
-    });
+      deleteButton.classList.add('mdl-button');
+      deleteButton.classList.add('mdl-js-button');
+      deleteButton.classList.add('mdl-button--accent');
+
+      // attach button to newLi
+      newLi.appendChild(deleteButton);
+
+      //attach the li to the ul
+      toDoList.appendChild(newLi);
+
+      // empty the input
+      newToDoText.value = '';
+
+      deleteButton.addEventListener('click', event => {
+        let liToRemove = event.target.parentNode;
+        liToRemove.remove();
+      });
+    }
   });
 }
 
